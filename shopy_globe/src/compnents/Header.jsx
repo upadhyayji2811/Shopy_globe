12fd3fcc42893382df5d41e70b7cfe5cf44a34a3
Prince Upadhyay <139058346+upadhyayji2811@users.noreply.github.com>
2025-05-10 17:00:45 +0530

    Add files via upload

---
+++ shopy_globe/src/compnents/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import shop from "../assets/shop.svg";
+
+const Header = () => {
+  return (
+    <div className="text-3xl">
+      <nav>
+        Shopy Globe <img src={shop} alt="" height />
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
